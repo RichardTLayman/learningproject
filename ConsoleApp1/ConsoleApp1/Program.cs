7a9793662a32d9e9a39ff176b023b8559e73e3d0
Richard Layman <richard.t.layman@gmail.com>
2019-02-28 11:27:53 -0500

    If applied, this commit will add optional param project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(increment(42, 2));
+        }
+
+        static int increment(int i, int incrementBy = 1)
+        {
+            return i + incrementBy;
+        }
+    }
+}
